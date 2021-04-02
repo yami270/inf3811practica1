d2c2836ee626dc4c69e1f38e8a4275be8433e2b2
yami270 <smma.fni@gmail.com>
2021-04-02 17:57:11 -0400

    Agregando ruta dinamica

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	return render_template('index.html')
+@app.route('/<name>')
+def index(name='Estudiante'):
+	return render_template('index.html', name=name)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=5001)
